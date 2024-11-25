702ca8655aa3b8f3d109df46ff4826ecc37ffc42
multiplex55 <multiplex55@yahoo.com>
2024-11-24 21:41:55 -0500

    proc to func

--- src/Year2020/Year2020_Day4Mod_Part2.nim
+++ src/Year2020/Year2020_Day4Mod_Part2.nim
@@ -2,13 +2,13 @@ import strutils, strformat, streams, os, times, tables, sequtils, algorithm,
         math
 import regex
 
-proc validate_byr(fieldVal: string): bool =
+func validate_byr(fieldVal: string): bool =
     fieldVal.parseInt >= 1920 and fieldVal.parseInt <= 2002
 
-proc validate_iyr(fieldVal: string): bool =
+func validate_iyr(fieldVal: string): bool =
     fieldVal.parseInt >= 2010 and fieldVal.parseInt <= 2020
 
-proc validate_eyr(fieldVal: string): bool =
+func validate_eyr(fieldVal: string): bool =
     fieldVal.parseInt >= 2020 and fieldVal.parseInt <= 2030
 
 func validate_hgt_in(fieldVal: string): bool =
@@ -19,31 +19,31 @@ func validate_hgt_cm(fieldVal: string): bool =
     let cleanedVal = fieldVal.replace("cm", "")
     return cleanedVal.parseInt >= 150 and cleanedVal.parseInt <= 193
 
-proc validate_hgt(fieldVal: string): bool =
+func validate_hgt(fieldVal: string): bool =
     if "cm" in fieldVal:
         return validate_hgt_cm(fieldVal)
     elif "in" in fieldVal:
         return validate_hgt_in(fieldVal)
     false
 
-proc validate_hcl(fieldVal: string): bool =
+func validate_hcl(fieldVal: string): bool =
     match(fieldVal, re2"\#[[:xdigit:]]{6}")
 
-proc validate_ecl(fieldVal: string): bool =
+func validate_ecl(fieldVal: string): bool =
     let validEyeColors = @["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
     fieldVal in validEyeColors
 
-proc validate_pid(fieldVal: string): bool =
+func validate_pid(fieldVal: string): bool =
     match(fieldVal, re2"[[:xdigit:]]{9}")
 
-proc hasAllRequiredKeys(passportTable: Table[string, string]): bool =
+func hasAllRequiredKeys(passportTable: Table[string, string]): bool =
     let requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
     for field in requiredFields:
         if not passportTable.hasKey(field):
             return false
     return true
 
-proc validatePassportAgainstFields(passportTable: Table[string, string]): bool =
+func validatePassportAgainstFields(passportTable: Table[string, string]): bool =
     var isValid = true
 
     if not hasAllRequiredKeys(passportTable):
@@ -71,7 +71,7 @@ proc validatePassportAgainstFields(passportTable: Table[string, string]): bool =
             return isValid
     isValid
 
-proc validatePassportTableWithValidKeys(passportTable: seq[Table[string,
+func validatePassportTableWithValidKeys(passportTable: seq[Table[string,
         string]]): int =
     var answer: int
     for pt in passportTable:
